ca61888b6b78f04600d78e50a42b881605555c49
Nsingletary <singletarybush@gmail.com>
2020-12-20 20:35:38 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,41 @@
+class Artist
+    @@all = []
+    attr_accessor :name, :songs
+  
+    def initialize(name)
+      @name = name
+      @songs = []
+      save
+    end
+  
+    def self.all
+      @@all    
+    end
+  
+    def add_song(song)
+      @songs << song
+      song.artist = self
+    end
+  
+    def songs
+      Song.all.select {|song| song.artist == self}
+    end
+  
+    def self.find_or_create_by_name(name)
+      self.find(name) ? self.find(name) : self.create(name)
+      self.find(name) ? self.find(name) : self.new(name)
+    end
+  
+    def self.find(name)
+      self.all.find {|artist| artist.name == name }
+    end
+  
+  
+    def self.create(name)
+      self.new(name).tap {|artist| artist.save}
+    end
+  
+    def save
+      @@all << self
+    end
+  
